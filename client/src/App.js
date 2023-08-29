312d1b8bf2f660c0d8b1b979973bc4582d6b316b
Tohir Bahromov <tohirbaxromov13@gmail.com>
2023-08-29 14:09:34 +0500

    asas

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,16 @@
 import {BrowserRouter, Routes, Route, HashRouter} from "react-router-dom";
 import axios from "axios";
-const Mainpage = lazy(()=> import("./pages/mainpage/mainpage"))
-const Register = lazy(()=> import("./pages/register/register"))
-const Login = lazy(()=> import("./pages/login/login"))
-const Elon = lazy(()=> import("./pages/elonlar/elon"))
-const Error = lazy(()=> import("./pages/error/error"))
-const News = lazy(()=> import("./pages/news/news"))
-const Singlenews = lazy(()=> import("./pages/news/singlenews"))
-const Profile = lazy(()=> import("./pages/profile/profile"))
-const Admin = lazy(()=> import("./pages/profile/profile"))
-import Loading from "./utils/loading/loading";
+import Mainpage from "./pages/mainpage/mainpage";
+import Register from "./pages/register/register";
+import Login from "./pages/login/login";
+import Elon from "./pages/elonlar/elon";
+import Error from "./pages/error/error";
+import News from "./pages/news/news";
+import Singlenews from "./pages/news/singlenews";
+import Profile from "./pages/profile/profile";
+import Admin from "./pages/admin/admin";
 import { useDispatch } from "react-redux";
-import React, { useEffect, Suspense, lazy } from "react";
+import React, { useEffect } from "react";
 import { getUserInfo } from "./redux/userSlice";
 function App() {
 
@@ -28,19 +27,17 @@ function App() {
   return (
     <>
       <HashRouter>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Routes>
-            <Route path="/" element={<Mainpage />}/>
-            <Route path="/register" element={<Register />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/announcements/:id" element={<Elon />} />
-            <Route path="/news" element={<News />} />
-            <Route path="/news/:id" element={<Singlenews />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/admin/dashboard" element={<Admin />} />
-            <Route path="*" element={<Error />} />
-          </Routes>
-        </Suspense>
+        <Routes>
+          <Route path="/" element={<Mainpage />}/>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/announcements/:id" element={<Elon />} />
+          <Route path="/news" element={<News />} />
+          <Route path="/news/:id" element={<Singlenews />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/admin/dashboard" element={<Admin />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
       </HashRouter>
     </>
   );
